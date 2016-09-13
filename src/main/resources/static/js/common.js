e995cf9795d0765e5eaac8e68e788795e0a63e69
wide-island777 <takaashi2525@gmail.com>
2016-09-13 16:22:17 +0900

    登録画面はモーダルで表示するように修正

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -3,12 +3,18 @@
  */
 
 $(document).ready(function() {
+	// datepicker作成
 	$('.datepicker').pickadate({
 		selectMonths: true, // Creates a dropdown to control month
 		selectYears: 15, // Creates a dropdown of 15 years to control year
 		format: 'yyyy/mm/dd',
-		formatSubmit: 'yyyy/mm/dd'
+		formatSubmit: 'yyyy/mm/dd',
+		closeOnSelect: true
 	});
-	
+	// セレクトボックス作成
 	$('select').material_select();
+
+	// モーダル作成
+	$('.modal-trigger').leanModal();
+	
 });
