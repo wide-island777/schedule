ae06e83db34db17f87bb318894c689cfc6f8c1d8
wide-island777 <takaashi2525@gmail.com>
2016-10-04 21:49:43 +0900

    ユーザ登録時にパスワードをエンコードして登録

--- src/main/java/com/hiro/shi/web/LoginController.java
+++ src/main/java/com/hiro/shi/web/LoginController.java
@@ -1,6 +1,9 @@
 package com.hiro.shi.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +15,8 @@ import com.hiro.shi.domain.service.UserService;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+	
+	private static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
 	@Autowired
 	UserService userService;
@@ -30,8 +35,12 @@ public class LoginController {
 
 	@RequestMapping(value = "save", method = RequestMethod.POST)
 	public String save(@RequestParam(value = "name") String name, @RequestParam(value = "password") String password) {
-		User account = new User();
-		userService.save(account);
+		User entryUser = new User();
+		entryUser.setName(name);
+		entryUser.setPassword(new BCryptPasswordEncoder().encode(password));
+		User user = userService.save(entryUser);
+		logger.info("entryUserName : " + user.getName());
+		logger.info("entryUserPass : " + user.getPassword());
 
 		return "login";
 	}
